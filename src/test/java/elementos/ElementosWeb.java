4af1c0ee85b5e8c4a681f08f18b68fab35c746a8
repoe2e <professores@e2etreinamentos.com.br>
2024-08-20 19:57:25 -0300

    inclusão do projeto

---
+++ src/test/java/elementos/ElementosWeb.java
@@ -0,0 +1,9 @@
+package elementos;
+
+import org.openqa.selenium.By;
+
+public class ElementosWeb {
+	
+	By btnLogin = By.xpath("//button[@class='login-button']");
+	By username = By.xpath("//input[@name='username']");
+}
