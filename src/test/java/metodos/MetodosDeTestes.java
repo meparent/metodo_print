4b63de1f544a43ba3b8e7ab863d9ecf419439ccc
paren@ <parenteme18@hotmail.com>
2024-09-28 03:24:13 -0300

    incluindo metodo print

--- src/test/java/metodos/MetodosDeTestes.java
+++ src/test/java/metodos/MetodosDeTestes.java
@@ -1,18 +1,53 @@
 package metodos;
 
+import java.io.File;
+import java.io.IOException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import drivers.Drivers;
 
 public class MetodosDeTestes extends Drivers {
-	
 	public void escrever(By elemento, String texto) {
-		
 		driver.findElement(elemento).sendKeys(texto);
-		
 	}
-	
+
 	public void clicar(By elemento) {
 		driver.findElement(elemento).click();
 	}
 
+	// as 3 linhas que preciso saber para implementar um método de Print Screm no
+	// meu método
+	// linha 1 public void tirarPrint (String nomeDoTeste, String nomeDaEvidencia) {
+	// desenvolvo metodo screm Print/não é obrigatório nome do teste/evidencia
+	// importar o file //fazer catsh
+	// linha 2 File srcFile = ((TakesScreenshot)
+	// driver).getScreenshotAs(OutputType.FILE); essa linha cria a FOTO
+	// fazer cash ou casth
+	// linha 3 FileUtils.copyFile(srcFile, new File("./evidencia/" + nomeDoTeste +
+	// "/" + nomeDaEvidencia + ".png"));
+	// "/" + nomeDaEvidencia + ".png"));
+
+	public void tirarPrint(String nomeDaFeature, String nomeDaEvidencia) {
+
+		File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+		try { // como pegar horário e adicionar na evidencia
+			FileUtils.copyFile(srcFile,
+					new File("./" + nomeDaFeature + "/" + nomeDaEvidencia + "_ " + timestamp + ".png"));
+
+			// logincomsucesso 20240903_200105.png
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+                   
 }
+                             
--- src/test/java/testes/LoginTeste.java
+++ src/test/java/testes/LoginTeste.java
@@ -1,6 +1,6 @@
 package testes;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import elementos.ElementosWeb;
 import metodos.MetodosDeTestes;
@@ -13,9 +13,30 @@ public class LoginTeste {
 	
 	
 	@Test
-	public void loginCampoSenhEmBranco() {
+	public void loginCampoSenhaEmBranco() {
 		Navegadores.iniciarTeste();
+		metodo.tirarPrint("Login/loginCampoSenhaEmBranco","Pagina inicial");
 		metodo.clicar(el.btnLogin);
+		metodo.tirarPrint("Login/loginCampoSenhaEmBranco","Tela de login");
+		metodo.clicar(el.btnContinuar); //se ele deu erro, os próximos passos não serão executados
+	    metodo.tirarPrint("Login/loginCampoSenhaEmBranco","mensagem de login dados em branco"); // botão direito encima regressivos
+	}                                                                                           //refresh- cria pasta evidencias na lateral/login
 	
-	}
+	
+                        //*************EXECUTADO COM SUCESSO******************//
+			
+	
+	@Test
+	public void loginDadosInvalidos() {
+		Navegadores.iniciarTeste();
+		metodo.tirarPrint("Login","loginDadosInvalidos-Pagina inicial");
+		metodo.clicar(el.btnLogin);
+		metodo.tirarPrint("Login", "loginDadosInvalidos-Tela de login");
+		metodo.escrever(el.username, "Mercedes");
+		metodo.escrever(el.password, "77777");
+		metodo.clicar(el.btnContinuar); //se ele deu erro, os próximos passos não serão executados
+	    metodo.tirarPrint("Login","loginloginDadosInvalidos-mensagem de login dados invalidos"); // botão direito encima regressivos
+	}      
+	//refresh- cria pasta evidencias na lateral/login
+	
 }
